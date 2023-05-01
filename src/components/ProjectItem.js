5367db6865e0982fe1a3f17cce96c83f11a07aad
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-01 23:48:12 +0300

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,20 @@
 import React from "react";
+import user from "../data";
+console.log(user)
 
 function ProjectItem({ name, about, technologies }) {
+  const myTechnologies = technologies.map((technology) => {
+    return <span key={technology}>{technology}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {myTechnologies}
       </div>
     </div>
   );
 }
-
 export default ProjectItem;
